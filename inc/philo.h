03e9a76eeda12ed04aa7701a95e7f15a068ceb0a
anmande <andremandevie@gmail.com>
2023-05-16 15:31:38 +0200

    test

---
+++ inc/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 11:57:32 by anmande           #+#    #+#             */
+/*   Updated: 2023/05/16 15:19:42 by anmande          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+int	ft_parseur(int argc, char **argv);
+int	ft_isdigit(char c);
+int	ft_checkarg(int argc, char **argv);
+
+#endif
